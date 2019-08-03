db43afb76ba61428cc607a13daba562c8a9bfbba
lilliejack05 <49662301+lilliejack05@users.noreply.github.com>
2019-08-02 23:53:51 -0500

    added config files and packages

---
+++ config/connection.js
@@ -0,0 +1,29 @@
+// pull in required dependencies
+var mysql = require("mysql");
+
+// create connection object
+var connection;
+
+// JawsDB on heroku
+if (process.env.JAWSDB_URL) 
+{ // localhost db
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+};
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ config/orm.js
@@ -0,0 +1,54 @@
+var connection = require("./connection");
+
+// turn object to a string
+function objToSql(ob) {
+    var arr = [];
+    for (var key in ob) {
+      if (Object.hasOwnProperty.call(ob, key)) {
+       arr.push(key + "=" + ob[key]);
+        }
+    }
+    return arr.toString();
+}
+    var orm = {
+        // select from table
+        selectAll: function(table, cb){
+            var queryString = "SELECT * FROM ??";
+            connection.query(queryString, [table], function(err, result) {
+                if (err) {
+                    throw err;
+                }
+                cb(result);    
+            });
+        }
+    }
+    
+    // insert into table
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table + " (" + cols.toString() + ") VALUES (?);"
+
+            //console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        }
+    )
+    // update based on id.
+    updateOne: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table + " SET " + objToSql(objColVals) + " WHERE " + condition + ";";
+
+       // console.log(queryString);
+
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }  
+            cb(result);
+        
+    });
+    
+module.exports = orm;
+    };
